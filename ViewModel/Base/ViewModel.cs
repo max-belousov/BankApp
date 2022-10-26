4c29a7372c43262a28c4f539202f70d035baa061
Максим Белоусов <max.belousov.ne@gmail.com>
2022-10-26 13:03:54 +0300

    Прописана логика ViewModel

--- ViewModel/Base/ViewModel.cs
+++ ViewModel/Base/ViewModel.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace BankWPFApp.ViewModel.Base
 {
-    internal class ViewModel
+    internal abstract class ViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string? PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string? PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
     }
 }
